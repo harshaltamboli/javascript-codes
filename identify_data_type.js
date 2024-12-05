4c12ab6af50e8233a9850c276a3a44bbce32e275
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-05 21:23:40 +0530

    this is a simple use in javascript operator

---
+++ identify_data_type.js
@@ -0,0 +1,21 @@
+let a="harshal";
+console.log(typeof(a),a);
+
+let b=5;
+console.log(typeof(b),b);
+
+let student =true;
+
+if(student== true){
+    console.log("you are talk a true");
+    
+}else{
+    console.log("you are not talk a true ");
+    
+}
+console.log(typeof(student));
+
+var c= null;
+console.log(typeof(null),c);
+
+
