bd42f006a80a288b89caffed1905a81316a57fb5
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-05 21:27:12 +0530

    this is a use crud operation in javascript

---
+++ crud_operation.js
@@ -0,0 +1,49 @@
+// push operation // insert op
+
+let fruits=['apple','mango','chicku','papiya']
+
+//fruits.push('kela');
+
+// console.log(fruits.push('sitafal'));
+
+// console.log(fruits);
+
+
+// // interview q push is return a lenght
+
+// //pop
+
+// // fruits.pop();
+// console.log(fruits.pop()); // check lenght
+
+// console.log(fruits);
+
+// //
+
+// // unshift
+
+// console.log(fruits.unshift("gangafal"));
+// console.log(fruits);
+
+
+// //shift
+
+// console.log(fruits.shift());
+// console.log(fruits);
+
+// // splice
+
+fruits.splice(1);
+fruits.splice(1,1, "paisa"); // 1,1 "adding a new iteam"
+//console.log(fruits.splice(1));
+console.log(fruits);
+
+
+// splice delate
+ fruits.splice(0,01,);
+ console.log(fruits);
+ 
+
+// replace splice
+ fruits.splice(1,2,'grapes');
+ console.log(fruits);
