8b4a7630b747601044d25b17ad1ef9430250b18c
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-06 20:02:30 +0530

    this is a simple use in javascript operator

---
+++ Data_type.js
@@ -0,0 +1,51 @@
+/* 
+let str='harshal';
+console.log(str);
+console.log(typeof str);
+*/
+
+// true our false 
+
+let myName = "harshal";
+if(myName)
+{
+    console.log('this is a true value');
+}
+else{
+    console.log('this is a falsy value');
+}
+
+
+//parseInt: Defination :- this is a used to parse a string and convert it to an
+// integer (whole no)
+
+
+//task
+/*const myNumber = "42.5";
+const myString = parseInt(myNumber);
+console.log(myString);
+*/
+
+//output:- 42;
+
+//task2
+/*
+const myString =42.5;
+const myNumber = parseFloat(myString);
+console.log(myNumber);
+*/
+
+//output :- 42.5;
+
+ //Diffrent between
+
+//both are diffrent between parseint is a can not store a floting 
+//value only dispaly a single value ex 42.5 is convert to 42 but
+// parseFloat is a store a same value in 42.5
+
+
+
+ // concanat value
+
+let myVariable="hashal"+"tamboli";
+console.log(myVariable);
