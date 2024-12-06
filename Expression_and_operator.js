18d1ef0dcfa11a22ec6fc66672e9551ecab9c500
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-06 20:03:35 +0530

    this is a simple use in javascript operator in expression

---
+++ Expression_and_operator.js
@@ -0,0 +1,43 @@
+let value = 'harshal';
+console.log(value);
+
+// Arithmatic operator
+
+// interview quesion
+var result ="hello" / 2;
+console.log(result);
+
+//output  NaN 
+
+
+
+var po =0.1 + 0.2;
+console.log(po);
+
+// output o.300000004
+
+//solution
+var pol = 0.1 +0.2;
+console.log(pol.toFixed(2));
+
+//output 0.30
+
+
+// comperision op
+
+console.log(5 == 5);
+
+//strict value 
+console.log(5 === 5);
+
+//output true
+
+console.log(5 != 5);
+//output false
+
+// Less than op
+console.log(5<7);
+
+// greter than op
+console.log(9>6)
+
