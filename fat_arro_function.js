3a1943838856cc8fd5d6f6e8494d2a42d66ec169
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-06 20:05:29 +0530

    this is a simple use in javascript fat arrow and anonimus function

---
+++ fat_arro_function.js
@@ -0,0 +1,11 @@
+const sum=(a,b)=>{
+    let result=`the sum og ${a} and ${b} is a : ${a+b}`;
+    console.log(result);
+}
+sum(5,3);
+
+
+// updation and optimiztion
+
+const sume=(a,b)=>console.log(`the sum og ${a} and ${b} is a : ${a+b}`);s ume(5,4);
+
