8c5f483757ef6b396fb80a8d455c4b81a9ac5856
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-07 20:30:38 +0530

    this is a simple use in javascript function expression

---
+++ function_expression.js
@@ -0,0 +1,19 @@
+let result = function home(name){
+
+
+    console.log("hello "+ name + ", welcome to my home ");
+}
+
+result("harshal" );
+result("Awaise");
+result("Washim");
+
+
+// Emidiatly call invokasion 
+
+(function(a,b){
+    console.log(a+b);
+
+    
+})
+(5,9);
