926faa787c3107a4b81a4affb28608e8edf5da03
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-08 09:33:48 +0530

    this is program are included in javascript for of array

---
+++ FOR_of_loop_array.js
@@ -0,0 +1,50 @@
+// creating array
+
+// using array constructor
+
+// let fruits= new Array(''apple','orange','banana'');
+// console.log(fruits);
+
+
+// using array literal
+
+// let fruits=['apple','orange','banana','mango'];
+// console.log(fruits);
+
+
+//
+
+// using empty array creted
+
+
+// let arr=[];
+// console.log(typeof arr);
+
+
+//modifying in array
+
+// let fru=["apple","mango","banana"]
+// console.log(fru[1]);
+// fru[1]="chicku";
+// console.log(fru);
+
+
+// for of loop
+
+
+// let fruits =["apple","mango","banana","papaiya","chicku"];
+
+// for(let iteam of fruits){
+//     console.log(iteam);
+// }
+// console.log(fruits[1]);
+
+
+// for in
+
+let fruits =["apple","mango","banana","papaiya","chicku"];
+
+for(let iteam in fruits){
+    console.log(iteam);
+    
+}
