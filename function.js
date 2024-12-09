385ec6199fbcb6beefb9279c5c947f07194123f0
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-09 12:06:07 +0530

    this is program is a simple function use in javascript

---
+++ function.js
@@ -0,0 +1,19 @@
+// function
+function wel(home){
+    console.log("hello " + home + ",welcome to my home");
+    
+}
+ wel("harshal");
+wel("awaise");
+wel("washim");
+
+
+
+
+ // number addition 
+function sum(a,b){
+    console.log(a+b);
+    
+}
+sum(5,2);
+sum(33,22); 
