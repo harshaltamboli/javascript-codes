b82f9f607be524a159dc7cbb5c91e5b1ded54705
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-09 12:10:34 +0530

    this is program is use basic operation in function

---
+++ math_object.js
@@ -0,0 +1,50 @@
+// math object
+
+const pivalue=Math.PI;
+console.log(pivalue);
+
+
+// Basic operation
+/// abs() is find a distance in 0 to curent number
+
+console.log(Math.abs(5));
+
+
+//Math.round()
+
+const roundvalue= Math.round(4.2)
+console.log(roundvalue);
+
+
+// output 5
+
+
+// math.ceil
+
+const ceilValue= Math.ceil(4.5)
+console.log(ceilValue);
+
+
+// output :- 5
+
+//Math.floor()
+
+const floorvalue= Math.floor(4.5)
+console.log(floorvalue);
+
+//output:- 4
+
+//math.trunc
+
+const truncvalue=Math.trunc(4.5);
+console.log(truncvalue);
+
+// output:-4
+const floorexample=Math.floor(-3.5);
+const trumcexample=Math.trunc(-3.5);
+console.log(floorexample);
+console.log(truncvalue);
+
+
+// 1st -4 this change the - value
+//sec  4 
