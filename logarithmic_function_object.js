5fa1b67422ba3c74a55a98e9195e8c611089a62b
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-09 12:08:43 +0530

    this is program is use function logarithmic like math,pow etc function are use in javascript

---
+++ logarithmic_function_object.js
@@ -0,0 +1,23 @@
+// Math.pow
+
+console.log(Math.pow(2,5));
+
+console.log(2**5);
+
+// Math.squre
+
+let squreRoot= Math.sqrt(25)
+console.log(squreRoot);
+
+// output : 5
+
+// Math.random() return a random number any time
+
+console.log(Math.random( 5*5 )/*    * 100  */);
+console.log(Math.round(Math.random()*100));
+
+
+
+
+
+
