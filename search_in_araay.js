e63c377691b91dc27289b0d1975bf6891b48e23f
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-09 12:12:51 +0530

    this is program is use inarray sear in array

---
+++ search_in_araay.js
@@ -0,0 +1,42 @@
+// indexOf searc in arry
+
+let number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+ let index= number.indexOf(4);
+console.log(index);
+
+
+// // output 3
+
+let indexx= number.indexOf(22);
+
+console.log(indexx);
+
+// output :- -1
+
+//tip: indexOf is return a index and 
+
+// supose not declare value print a user so output isalways -1
+
+
+//lastindex in search 
+
+
+let numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,4,14,15];
+let Indexxx= numbers.lastIndexOf(4);
+
+console.log(Indexxx);
+
+//output is :- 13
+
+
+
+
+//includes
+
+let Number=[1,2,3,4,5,6,7,8,9,10,11,12,13,4,14,15];
+let indexxx= Number.includes(50);
+
+console.log(indexxx);
+
+
+// output:- true 
