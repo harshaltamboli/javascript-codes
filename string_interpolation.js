0adc9b3b6d386ec34342c6be8459faae56969c7c
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-09 12:16:25 +0530

    this is program is use string in javascript

---
+++ string_interpolation.js
@@ -0,0 +1,16 @@
+//  template leteralce
+
+// let fname = "harsal";
+// let lname = "tamboli";
+
+// let fullname=`${fname}  ${lname}`;
+// console.log(fullname);
+
+
+
+//advance in string interppolation
+
+let age=21;
+
+let message=`I am ${age} year old`;
+console.log(message);
