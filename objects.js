bec86dc01de0eab1cccb8a4f6d2e8d6bde32fa5c
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-13 14:30:20 +0530

    this is program a proram use in javascript object only

---
+++ objects.js
@@ -0,0 +1,9 @@
+const student ={
+    fullname:"harshal tamboli",
+    marks:78.80,
+    printmarks: function(){
+        console.log("marks=",this.marks);
+        
+    }
+}
+//console.log(student);
