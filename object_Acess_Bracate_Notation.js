fc0bdf102c0955ca14f8440195dbac1acfb5f379
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-14 14:39:18 +0530

    this program is use a object in literls object and curly bracket notation in javascript

---
+++ object_Acess_Bracate_Notation.js
@@ -0,0 +1,25 @@
+ 
+ // leterals in objects
+ const student ={
+    name:"harshal",
+    "fullName":"Harshal tamboli",
+    age:20,
+    email:"harshaltamboli429@mail.com",
+    mo:7448085013,
+    Gender:"Femail"
+ };
+
+ // Accessing in objecs
+
+ //  Bracket Notation
+
+ console.log(student['fullName']);
+
+ //Dot notation
+ // dot notation is not access in string type .string type access in only bracket notation
+
+ //error line
+ //console.log(student."fullName");
+ console.log(student.age);
+proto;
+ 
