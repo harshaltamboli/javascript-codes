80d866e85f46e86042de8963fae4dce04918ef68
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-14 14:37:02 +0530

    this program is use a object prototype in javascript

---
+++ prototype.js
@@ -0,0 +1,23 @@
+const virat={
+
+    name:"virat kohli",
+    age:32,
+    run:90
+};
+
+const rohit={
+    
+    name:"rohit sharma",
+    age:31,
+    run:56
+};
+
+const dhoni={
+    
+    name:"ms dhoni",
+    age:40,
+    run:78
+};
+
+
+dhoni.__proto__ = virat;
