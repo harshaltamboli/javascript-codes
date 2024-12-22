35776c77db70333dec69b309443d76738786a996
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-22 10:20:13 +0530

    this program is use solve a task in array for Each method calculate a squre in number

---
+++ squre_find_in_array.js
@@ -0,0 +1,15 @@
+// Print the squares of all numbers in the array [1, 2, 3, 4, 5].
+// Hint: Multiply each element with itself.
+
+
+let a=[1,2,3,4,5];
+let empty=[];
+a.forEach((current_value,index,array) =>{
+
+// empty.push(current_value*current_value);
+
+console.log(`${current_value} = ${current_value * current_value}`);
+
+});
+// console.log(empty);
+
