348bfbbd5863e3691cdd2c89cacd821ee82ab3e2
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-23 21:26:06 +0530

    this program is use solve a task in if else include a teprechare find

---
+++ temprechare.js
@@ -0,0 +1,16 @@
+// Task 5: Temperature Check
+// Description: Ek program likhiye jo check kare ki temperature freezing 
+// point se neeche hai ya nahi.
+// Hint:
+// Freezing point 0 degrees Celsius hai. Agar temperature
+//  0 se neeche hai, to "It's freezing!" print karein.
+
+
+let temperature=-1;
+if(temperature <=0){
+    console.log(`${temperature} it's freezing`);
+    
+}else{
+    console.log(" it is a high tempreture");
+    
+}
