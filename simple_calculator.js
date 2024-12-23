f8135399ff9e9aacc94327641f69775c98c11f9e
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-23 21:27:54 +0530

    this program is use solve a task in simple claculator create in javascript

---
+++ simple_calculator.js
@@ -0,0 +1,29 @@
+
+let a=10;
+let b=5;
+let op=prompt("perform operation choose symbol ");
+
+
+switch(op){
+    case '+':
+        console.log(a+b +" ADDITION");
+        break;
+
+        case '-':
+            console.log(a-b + " SUBTRACTION");
+            break;
+            case '*':
+                console.log(a*b+" MULTIPLICATION");
+                break;
+
+                case '/':
+                    console.log(a/b + " DIVISION");
+                    break;
+
+                    default:
+                        console.log(" please valid a simbol");
+                        
+                    
+                
+
+}
