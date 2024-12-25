50f008082e5fe23f73049bf350f5d1f80c72b819
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-25 09:15:48 +0530

    this program is use else if statement find the age in person

---
+++ else_if_statement.js
@@ -0,0 +1,26 @@
+let age=89;
+
+if(age<=18){
+    console.log("child");
+    
+}
+
+    else if(age>18 && age<25){
+        console.log("yungest");
+        
+    }
+
+
+    else if(age>25 && age< 50){
+        console.log("midle");
+        
+    }
+    else if(age>50 && age<100){
+        console.log("oldest");
+        
+    }
+else{
+    console.log("not match in age plese enter the valid age");
+    
+
+}
