71a7bda0287ed883392d8f9d95b3090d6fb93d69
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-29 15:30:28 +0530

    this prorm is use a function and print a vowel in string and count a strings

---
+++ vowel_Count.js
@@ -0,0 +1,17 @@
+
+function vowell(str){
+
+    let cout=0;
+    for( const char of str){
+        if(char=== "a"||char==="e"||char==="i"|| char==="o"|| char==="u"){
+
+            cout++
+        }
+        
+        
+    }
+    console.log(cout);
+    
+}
+vowell("harshal");
+
