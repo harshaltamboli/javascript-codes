28fe0b0b068262dbf10d2b03c2c1b41f0996ee17
harshaltamboli <harshaltamboli429@gmail.com>
2025-01-04 15:48:08 +0530

    this prorm is a win

---
+++ Name_win_game.js
@@ -0,0 +1,25 @@
+// let win= "harshal"
+//   let gest = prompt("Choose the valid winner","","awaise","vasim","harshal",);
+
+ 
+//  while(win !=gest){
+//     gest=prompt("this is not win try again");
+
+//  }
+//  console.log("congratulation you are win");
+ 
+
+
+let fullName;
+fullName=prompt("Enter The Full Name");
+console.log("@"+fullName+fullName.length);
+
+
+// let win = "harshal";
+// let gest = prompt("Choose the valid winner: (awaise, vasim, harshal)");
+
+// while (win !== gest) {
+//   gest = prompt("This is not correct. Try again: (awaise, vasim, harshal)");
+// }
+
+// console.log("Congratulations! You guessed the winner!");
